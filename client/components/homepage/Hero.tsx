4789862ec6015b8bbf99465f4db8d69398507342
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-27 22:23:18 +0600

    hero_Done

--- client/components/homepage/Hero.tsx
+++ client/components/homepage/Hero.tsx
@@ -23,9 +23,4 @@ function Hero() {
 
 export default Hero;
 
-{/* <h5>Welcome to</h5>
-      <h1>SWE SOCIETY</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p> */}
+
