41390a9d681b70e682ab9ea7b67d97611211016e
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-27 22:05:57 +0600

    animated_spot_light

--- client/components/homepage/Hero.tsx
+++ client/components/homepage/Hero.tsx
@@ -1,6 +1,11 @@
+import { Spotlight } from "../ui/Spotlight";
 function Hero() {
   return (
     <div>
+       <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
       <h5>Welcome to</h5>
       <h1>SWE SOCIETY</h1>
       <p>
