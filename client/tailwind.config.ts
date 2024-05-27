36f12b61e9ed6febc108dd287cedf56c6ed56831
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-27 23:32:16 +0600

    moving_card_with_dummy_data

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -69,6 +69,11 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" },
@@ -89,6 +94,8 @@ const config = {
         },
       },
       animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
         spotlight: "spotlight 2s ease .75s 1 forwards",
