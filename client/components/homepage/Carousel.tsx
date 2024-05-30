3014652373f3ae637beae9815ea013c224fa2b52
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-30 23:31:52 +0600

    added_globe_in_hero_section

--- client/components/homepage/Carousel.tsx
+++ client/components/homepage/Carousel.tsx
@@ -40,7 +40,7 @@ function Carousel() {
     <div className="h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
      <div >
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-      <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+      <div className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
 
 
 
@@ -55,7 +55,7 @@ function Carousel() {
   
 
 
-      </p>
+      </div>
       </div>
     </div>
   );
--- client/components/homepage/Hero.tsx
+++ client/components/homepage/Hero.tsx
@@ -1,4 +1,5 @@
 import { Spotlight } from "../ui/Spotlight";
+import { GlobeDemo } from "./Globe_demo";
 function Hero() {
   return (
     <div className='h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0'>
@@ -8,12 +9,16 @@ function Hero() {
       className="-top-40 left-0 md:left-60 md:-top-20"
       fill="white"
     />
-      <h1 className='mt-30 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Welcome to SWE SOCIETY</h1>
+      {/* <h1 className='mt-30 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400'>Welcome to SWE SOCIETY</h1>
       <p className='mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
     
-      </p>
+      </p> */}
+      <div className='mt-4 flex flex-col items-center justify-center gap-4 md:
+      flex-row md:gap-8 md:justify-start md:items-start'>
+      <GlobeDemo/>
+      </div>
 
     </div>
    
