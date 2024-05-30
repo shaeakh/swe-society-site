44176a79938174848d481e2699574dcd7ed99208
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-30 23:42:10 +0600

    notice_card_Canvas_reveal

--- client/components/homepage/NoticeSection.tsx
+++ client/components/homepage/NoticeSection.tsx
@@ -1,9 +1,9 @@
-import NoticeCard from "../noticespage/NoticeCard";
+import { NoticeCard } from "../noticespage/NoticeCard";
 
 function NoticeSection() {
   return (
     <div>
-      <h1>Notices</h1>
+      <h1 className="flex flex-col justify-center items-center">Notices</h1>
       <NoticeCard />
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
