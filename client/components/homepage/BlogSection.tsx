88469049ca278d5f64e077e5c5efc8a721dc80d0
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-30 23:57:53 +0600

    blog_card_with_CARD_STACK

--- client/components/homepage/BlogSection.tsx
+++ client/components/homepage/BlogSection.tsx
@@ -1,10 +1,12 @@
-import BlogCard from "../blogspage/BlogCard";
-
+import {BlogCard} from "../blogspage/BlogCard";
+import Link from "next/link";
 function BlogSection() {
   return (
     <div>
-      <h1>Blogs</h1>
+      <h1 className="flex flex-col items-center">Blogs</h1>
+      
       <BlogCard />
+      
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
   );
