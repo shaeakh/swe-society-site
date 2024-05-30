d2b142c23ca50e1c95cad534585c7dc18cdca803
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-31 00:26:40 +0600

    achievement_card_with_BackgroundGradient

--- client/components/achievementspage/AchievementCard.tsx
+++ client/components/achievementspage/AchievementCard.tsx
@@ -1,5 +1,91 @@
-function AchievementCard() {
-  return <div>This is single Achievement Card</div>;
+"use client";
+import React from "react";
+import { BackgroundGradient } from "../ui/background-gradient";
+import { IconAppWindow } from "@tabler/icons-react";
+import Image from "next/image";
+
+export function AchievemenCard() {
+  return (
+    <div className="flex">
+      <div className="p-8">
+      <BackgroundGradient className=" rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`/https://t4.ftcdn.net/jpg/03/88/30/69/360_F_388306986_HNTycrIKQQ3aSkce0Vod4WoESHedMmHT.jpg`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+          Achievement 1
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+          February 17, 2024. Your best opportunity to get these right now is by
+          entering raffles and waiting for the official releases.
+        </p>
+        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+          <span>See more </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            $100
+          </span>
+        </button>
+      </BackgroundGradient>
+      </div>
+      <div className="p-8">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`/https://t4.ftcdn.net/jpg/03/88/30/69/360_F_388306986_HNTycrIKQQ3aSkce0Vod4WoESHedMmHT.jpg`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Achievement 2
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+          February 17, 2024. Your best opportunity to get these right now is by
+          entering raffles and waiting for the official releases.
+        </p>
+        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+          <span>See more </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            $100
+          </span>
+        </button>
+      </BackgroundGradient>
+      </div>
+      <div className="p-8">
+      <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+        <Image
+          src={`/https://t4.ftcdn.net/jpg/03/88/30/69/360_F_388306986_HNTycrIKQQ3aSkce0Vod4WoESHedMmHT.jpg`}
+          alt="jordans"
+          height="400"
+          width="400"
+          className="object-contain"
+        />
+        <p className="text-base sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">
+        Achievement 3
+        </p>
+
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          The Air Jordan 4 Retro Reimagined Bred will release on Saturday,
+          February 17, 2024. Your best opportunity to get these right now is by
+          entering raffles and waiting for the official releases.
+        </p>
+        <button className="rounded-full pl-4 pr-1 py-1 text-white flex items-center space-x-1 bg-black mt-4 text-xs font-bold dark:bg-zinc-800">
+          <span>See more </span>
+          <span className="bg-zinc-700 rounded-full text-[0.6rem] px-2 py-0 text-white">
+            $100
+          </span>
+        </button>
+      </BackgroundGradient>
+      </div>
+    </div>
+  );
 }
 
-export default AchievementCard;
--- client/components/homepage/AchievementSection.tsx
+++ client/components/homepage/AchievementSection.tsx
@@ -1,11 +1,11 @@
-import AchievementCard from "../achievementspage/AchievementCard";
+import { AchievemenCard } from "../achievementspage/AchievementCard";
 
 function AchievementSection() {
   return (
     <div>
-      <h1>Achievements</h1>
-      <AchievementCard />
-      <button>See More</button>
+      <h1 className="flex flex-col items-center">Achievements</h1>
+      <AchievemenCard />
+      
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
   );
