ad82a605ae8d53d279d107ebeef4250464d2c7bf
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-30 23:49:04 +0600

    event-card_card-hover_effect

--- client/components/eventspage/EventCard.tsx
+++ client/components/eventspage/EventCard.tsx
@@ -1,5 +1,49 @@
-function EventCard() {
-  return <div>This is single event EventCard</div>;
-}
+'use client'
 
-export default EventCard;
+import { HoverEffect } from "../ui/card-hover-effect";
+
+export function EventCard() {
+  return (
+    <div className="max-w-5xl mx-auto px-8">
+      <HoverEffect items={projects} />
+    </div>
+  );
+}
+export const projects = [
+  {
+    title: "Event 1",
+    description:
+      "A technology company that builds economic infrastructure for the internet.",
+    link: "https://stripe.com",
+  },
+  {
+    title: "Event 2",
+    description:
+      "A streaming service that offers a wide variety of award-winning TV shows, movies, anime, documentaries, and more on thousands of internet-connected devices.",
+    link: "https://netflix.com",
+  },
+  {
+    title: "Event 3",
+    description:
+      "A multinational technology company that specializes in Internet-related services and products.",
+    link: "https://google.com",
+  },
+  {
+    title: "Event 4",
+    description:
+      "A technology company that focuses on building products that advance Facebook's mission of bringing the world closer together.",
+    link: "https://meta.com",
+  },
+  {
+    title: "Event 5",
+    description:
+      "A multinational technology company focusing on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    link: "https://amazon.com",
+  },
+  {
+    title: "Event 6",
+    description:
+      "A multinational technology company that develops, manufactures, licenses, supports, and sells computer software, consumer electronics, personal computers, and related services.",
+    link: "https://microsoft.com",
+  },
+];
--- client/components/homepage/EventSection.tsx
+++ client/components/homepage/EventSection.tsx
@@ -1,9 +1,9 @@
-import EventCard from "../eventspage/EventCard";
+import {EventCard} from "../eventspage/EventCard";
 
 function EventSection() {
   return (
     <div>
-      <h1>Events</h1>
+      <h1 className="flex flex-col items-center">Events</h1>
       <EventCard />
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
     </div>
