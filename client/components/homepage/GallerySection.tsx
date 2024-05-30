cc0e6b742fef9e676d1d8ec045b19b06b4c76433
Arnob-sen <106549897+Arnob-sen@users.noreply.github.com>
2024-05-31 00:38:54 +0600

    Gallery_push_3D_card

--- client/components/homepage/GallerySection.tsx
+++ client/components/homepage/GallerySection.tsx
@@ -1,9 +1,9 @@
-import GalleryCard from "../gallerypage/GalleryCard";
+import {GalleryCard} from "../gallerypage/GalleryCard";
 
 function GallerySection() {
   return (
     <div>
-      <h1>Gallery</h1>
+      <h1 className="flex flex-col items-center">Gallery</h1>
       <GalleryCard />
       <button>See More</button>
       <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700" />
